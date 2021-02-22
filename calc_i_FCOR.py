1d7f4b966bef5b6ddc91fb8efb53e56b09293d36
hcaiafa <henrique.caiafa@engenharia.ufjf.br>
2021-02-22 18:56:00 -0300

    adding max and min plots based on std

--- calc_i_FCOR.py
+++ calc_i_FCOR.py
@@ -15,7 +15,9 @@ with open('Rmat.pickle', 'rb') as filehandle:
     
 U, s, V = np.linalg.svd(Rmat)
 
-s_inv = np.array([1/i for i in s if i > 1e-5])
+threshold = 1e-5
+
+s_inv = np.array([1/i if i > threshold else 0. for i in s])
 
 W_inv = np.zeros((len(V), len(U))) #, dtype=complex)
 
@@ -44,11 +46,18 @@ _,ns = xy_read.shape
 
 t = np.linspace(0.,ns*Ts,ns)
 
-plt.figure(1)
-plt.plot(t,xy_read[0],t,xy_read[1])
-
 kick_factor = 0.03 # urad/mA
 corr_signals = R_pseudoinv @ xy_read/kick_factor 
+
+# plot correctors' time signals (largest std vs lowest std value)
+corr_signals_std = np.std(corr_signals,axis=1)
+
+min_index = np.argmin(corr_signals_std)
+max_index = np.argmax(corr_signals_std)
+
+plt.figure(1)
+plt.plot(t,corr_signals[max_index],t,corr_signals[min_index])
+
 
 freqs = np.fft.fftfreq(ns)*fFOFB
 
@@ -56,15 +65,24 @@ mask = freqs > 0  # list with indexes
 
 cur_spectra =  np.array( [ 2.0*np.abs(np.fft.fft(corr_signals[i])/ns) for i in range(n_fcor) ] )
 
+# mean, max and min collumn-wise (i.e. for each fft bin)
+cur_mean = np.mean(cur_spectra,axis=0)
+#cur_max = np.max(cur_spectra,axis=0)
+#cur_min = np.min(cur_spectra,axis=0)
+cur_max = cur_spectra[max_index]
+cur_min = cur_spectra[min_index]
+
+'''
 cur_mean =  np.array([ np.mean(cur_spectra[:,i]) for i in range(ns) ] )
 cur_max =  np.array([ np.max(cur_spectra[:,i]) for i in range(ns) ] )
 cur_min =  np.array([ np.min(cur_spectra[:,i]) for i in range(ns) ] )
+'''
 
 plt.figure(2)
 
-plt.plot(freqs[mask],cur_max[mask],label="max spectrum")
+plt.plot(freqs[mask],cur_max[mask],label="max std spectrum")
 plt.plot(freqs[mask],cur_mean[mask],label="avg spectrum")
-plt.plot(freqs[mask],cur_min[mask],label="min spectrum")
+plt.plot(freqs[mask],cur_min[mask],label="min std spectrum")
 plt.legend()
 plt.yscale('log')
 plt.ylabel('Amplitude [mA]')
